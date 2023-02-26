127d303e6da7043363a4870fd19d718389469ca1
Menashe <menasheho@gmail.com>
2023-02-27 00:27:44 +0200

    add register page

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskDelete, TaskList, TaskDetail, TaskCreate, TaskUpdate, CustomLoginView
+from .views import TaskDelete, TaskList, TaskDetail, TaskCreate, TaskUpdate, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 
@@ -7,6 +7,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page = 'login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
   
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
